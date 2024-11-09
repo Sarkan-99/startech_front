867827501c778c1f3ca7470ae7f0c13b0682301f
maste <masterfox2003@gmail.com>
2024-11-09 11:14:26 +0100

    youssef layout1

--- app/auth/page.jsx
+++ app/auth/page.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+'use client';
+import React from 'react';
 
+import { Card } from 'primereact/card';
+import { Divider } from 'primereact/divider';
+import { Button } from 'primereact/button';
+     
 const page = () => {
   return (
-    <div>page</div>
-  )
+    <div className="flex justify-center items-center h-screen">
+            <Card className='w-auto h-auto'>
+                <div className='flex flex-row flex justify-center items-center h-auto'>
+                <span className="m-4">Login</span>
+                <Divider layout="vertical"/>
+                <div className="text-center grid">
+                    <form>
+                        <div>
+                            <Button outlined severity="secondary">
+                                <div className="flex justify-center items-center">
+                                <img alt="logo" src="https://img.icons8.com/ios-filled/50/google-logo.png" className="mr-3" height={25}></img>
+                                <span className="font-medium">Se connecter avec Google</span>
+                            </div>
+                            </Button>
+                        </div>
+                    </form>
+                </div>
+                </div>
+            </Card> 
+    </div>
+    );
 }
 
 export default page
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Header from "./components/header/page";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -21,9 +22,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased p-0 m-0 flex flex-col min-h-screen`}
       >
-        {children}
+        <Header />
+        {/* Content fills the remaining screen height */}
+        <main className="flex-grow">{children}</main>
       </body>
     </html>
   );
