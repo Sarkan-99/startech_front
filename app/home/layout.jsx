a6c58ff1ce01d6fa60bc50b3344ffcb84e5c26df
maste <masterfox2003@gmail.com>
2024-11-11 12:56:42 +0100

    Logo dezign and more etc... from MFox (youssef)

--- app/home/layout.jsx
+++ app/home/layout.jsx
@@ -7,19 +7,22 @@ import Link from 'next/link';
 import { Button } from 'primereact/button';
 import { Divider } from 'primereact/divider';
 import { OverlayPanel } from 'primereact/overlaypanel';
+import { Image } from 'primereact/image';
+
 
 
 const mainpage = ({children}) => {
 
   const op = useRef(null);
 
+
   
   
   return (
     <div className='relative bg-zinc-50 h-screen p-0 m-0'>
       <div className='absolute flex flex-row w-full bg-blue-500 p-2'>
-        <i className="pi pi-apple p-5">LOGO</i>
-        <div className="absolute flex justify-center p-1 right-3" >
+      <Image src="Logo.png" alt="Logo Image" width="110" className='m-2 ml-5' />
+      <div className="absolute flex justify-center p-1 right-3" >
                   <Button className='my-custom-button' icon="pi pi-user" rounded  aria-label="User" onClick={(e) => op.current.toggle(e)} />
                   <OverlayPanel ref={op}>
                       <div className="flex flex-col">
--- app/home/page.jsx
+++ app/home/page.jsx
@@ -15,7 +15,6 @@ const Page = () => {
     const [activeProjects, setActiveProjects] = useState([]);
     const [inactiveProjects, setInactiveProjects] = useState([]);
 
-<<<<<<< HEAD
     useEffect(() => {
       const fetchProjects = async () => {
         try {
@@ -33,70 +32,12 @@ const Page = () => {
           console.error('Error fetching projects: ', error);
       }
       };
-=======
-    // Different datasets for each tab
-    const dashboardData = [
-        { id: 1, name: 'John Doe', country: { name: 'USA' }, representative: { name: 'Amy Elsner' }, status: 'qualified' },
-        { id: 2, name: 'Jane Smith', country: { name: 'Germany' }, representative: { name: 'Elwin Sharvill' }, status: 'new' },
-        { id: 3, name: 'Alex Johnson', country: { name: 'Brazil' }, representative: { name: 'Asiya Javayant' }, status: 'renewal' },
-        { id: 4, name: 'Alex Johnson', country: { name: 'Brazil' }, representative: { name: 'Asiya Javayant' }, status: 'renewal' },
-        { id: 5, name: 'Alex Johnson', country: { name: 'Brazil' }, representative: { name: 'Asiya Javayant' }, status: 'renewal' },
-        { id: 6, name: 'Alex Johnson', country: { name: 'Brazil' }, representative: { name: 'Asiya Javayant' }, status: 'renewal' },
-        { id: 7, name: 'Alex Johnson', country: { name: 'Brazil' }, representative: { name: 'Asiya Javayant' }, status: 'renewal' },
-        { id: 8, name: 'Alex Johnson', country: { name: 'Brazil' }, representative: { name: 'Asiya Javayant' }, status: 'renewal' },
-
-    
-    ];
-
-    const transactionData = [
-        { id: 4, name: 'Chris Black', country: { name: 'UK' }, representative: { name: 'Stephen Shaw' }, status: 'negotiation' },
-        { id: 5, name: 'Lucy Brown', country: { name: 'Canada' }, representative: { name: 'Onyama Limba' }, status: 'unqualified' },
-        { id: 6, name: 'Emma Wilson', country: { name: 'France' }, representative: { name: 'Ivan Magalhaes' }, status: 'qualified' }
-    ];
-
-    // Select the dataset based on the active tab
-    const customers = activeIndex === 0 ? dashboardData : transactionData;
-
-    const statuses = ['unqualified', 'qualified', 'new', 'negotiation', 'renewal'];
-
-    const getSeverity = (status) => {
-        switch (status) {
-            case 'unqualified':
-                return 'danger';
-            case 'qualified':
-                return 'success';
-            case 'new':
-                return 'info';
-            case 'negotiation':
-                return 'warning';
-            default:
-                return null;
-        }
-    };
-
-    const countryBodyTemplate = (rowData) => (
-        <div className="flex align-items-center gap-2">
-            <span>{rowData.country.name}</span>
-        </div>
-    );
-
-    const representativeBodyTemplate = (rowData) => (
-        <div className="flex align-items-center gap-2">
-            <span>{rowData.representative.name}</span>
-        </div>
-    );
-
-    const statusBodyTemplate = (rowData) => (
-        <Tag value={rowData.status} severity={getSeverity(rowData.status)} />
-    );
->>>>>>> 7c7284f75f317ba2dc7211caaec50913b97b944f
 
       fetchProjects();
     }, []);
 
     return (
         <div className='card'>
-<<<<<<< HEAD
             <TabView  >
               <TabPanel header="Non-notéé">
                 <CustomTable data={inactiveProjects}/>
@@ -105,23 +46,6 @@ const Page = () => {
                 <CustomTable data={activeProjects}/>
               </TabPanel>
             </TabView>
-=======
-            <div className="card flex justify-center">
-                <TabMenu model={items} activeIndex={activeIndex} onTabChange={(e) => setActiveIndex(e.index)} />
-            </div>
-
-            <div className="card">
-                <DataTable value={customers} paginator rows={5} filters={filters} onFilter={(e) => setFilters(e.filters)}
-                    selection={selectedCustomer} onSelectionChange={(e) => setSelectedCustomer(e.value)} selectionMode="single" dataKey="id"
-                    emptyMessage="No customers found." tableStyle={{ minWidth: '50rem' }}>
-                    
-                    <Column field="name" header="Name" sortable  style={{ width: '25%' }}></Column>
-                    <Column header="Owner" body={countryBodyTemplate} sortable style={{ width: '25%' }}></Column>
-                    <Column header="Activity" body={representativeBodyTemplate} sortable style={{ width: '25%' }}></Column>
-                    <Column field="status" header="Status" body={statusBodyTemplate} sortable style={{ width: '25%' }}></Column>
-                </DataTable>
-            </div>
->>>>>>> 7c7284f75f317ba2dc7211caaec50913b97b944f
         </div>
     );
 };
